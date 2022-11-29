01678e5d6335279f58f8721eb2d5c2f1389d947b
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-11-29 10:16:53 +0330

    change pdp routing

--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./index.scss";
 import "remixicon/fonts/remixicon.css";
@@ -8,23 +8,19 @@ import "remixicon/fonts/remixicon.css";
 import Footer from "home/Footer";
 import Header from "home/Header";
 
-import ErrorBoundary from "./ErrorBoundary";
 import ProductDetail from "./ProductDetail";
 
-const router = createBrowserRouter([
-  {
-    path: "/product/:id",
-    element: <ProductDetail />,
-  },
-]);
-
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <ErrorBoundary>
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
       <Header />
-    </ErrorBoundary>
-    <RouterProvider router={router} />
-    <Footer />
-  </div>
+      <div className="my-10">
+        <Routes>
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
